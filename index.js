02c5b6087af8634132af11d7a4a8a4180ecc7f8f
Gyorgy Balassy <gybalassy@logmein.com>
2015-06-08 16:15:04 +0200

    CHANGED: Using allure-js-commons 1.0.2. Upgraded to 1.1.0.

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
 var Base = require('mocha').reporters.Base,
     Allure = require('allure-js-commons'),
+    allureReporter = new Allure(),
+    Runtime = require('allure-js-commons/runtime'),
     log = console.log;
 
-
+global.allure = new Runtime(allureReporter);
 module.exports = AllureReporter;
 
 
 function AllureReporter(runner, opts) {
     Base.call(this, runner);
-    var options = (opts && opts.reporterOptions) || {},
-        allure = new Allure(options);
+    allureReporter.setOptions(opts.reporterOptions);
 
     runner.on('suite', function (suite) {
-        allure.startSuite(suite.fullTitle());
-
+        allureReporter.startSuite(suite.fullTitle());
+        
         if (suite.root) return;
         suite.startDate = Date.now();
         log("##teamcity[testSuiteStarted name='" + escape(suite.title) + "']");
     });
 
-    runner.on('suite end', function (suite) {
-        allure.endSuite(suite.fullTitle());
 
+    runner.on('suite end', function (suite) {
+        allureReporter.endSuite();
+        
         if (suite.root) return;
         log("##teamcity[testSuiteFinished name='" + escape(suite.title) + "' duration='" + (Date.now() - suite.startDate) + "']");
     });
 
+
     runner.on('test', function(test) {
-        allure.startCase(test.parent.fullTitle(), test.title);
+        allureReporter.startCase(test.title);
         log("##teamcity[testStarted name='" + escape(test.title) + "' captureStandardOutput='true']");
     });
 
     runner.on('pending', function(test) {
-        allure.pendingCase(test.parent.fullTitle(), test.title);
+        allureReporter.pendingCase(test.title);
         log("##teamcity[testIgnored name='" + escape(test.title) + "' message='pending']");
     });
 
     runner.on('pass', function(test) {
-        allure.endCase(test.parent.fullTitle(), test.title, 'passed');
+        allureReporter.endCase('passed');
         log("##teamcity[testFinished name='" + escape(test.title) + "' duration='" + test.duration + "']");
     });
 
     runner.on('fail', function(test, err) {
         var status = err.name === 'AssertionError' ? 'failed' : 'broken';
-        allure.endCase(test.parent.fullTitle(), test.title, status, err);
+        if(global.onError) {
+            global.onError(err);
+        }
+        allureReporter.endCase(status, err);
         log("##teamcity[testFailed name='" + escape(test.title) + "' message='" + escape(err.message) + "' captureStandardOutput='true']");
     });
 }
