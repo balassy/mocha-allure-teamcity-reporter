691366067fb099e9d1597a2bb6c6d965df82fcb4
Gyorgy Balassy <Gyorgy.Balassy@logmein.com>
2016-05-07 05:04:46 +0200

    FORMATTED: Reformatted the file with WebStorm.

--- index.js
+++ index.js
@@ -14,39 +14,37 @@ function AllureReporter(runner, opts) {
 
     runner.on('suite', function (suite) {
         allureReporter.startSuite(suite.fullTitle());
-        
+
         if (suite.root) return;
         suite.startDate = Date.now();
         log("##teamcity[testSuiteStarted name='" + escape(suite.title) + "']");
     });
 
-
     runner.on('suite end', function (suite) {
         allureReporter.endSuite();
-        
+
         if (suite.root) return;
         log("##teamcity[testSuiteFinished name='" + escape(suite.title) + "' duration='" + (Date.now() - suite.startDate) + "']");
     });
 
-
-    runner.on('test', function(test) {
+    runner.on('test', function (test) {
         allureReporter.startCase(test.title);
         log("##teamcity[testStarted name='" + escape(test.title) + "' captureStandardOutput='true']");
     });
 
-    runner.on('pending', function(test) {
+    runner.on('pending', function (test) {
         allureReporter.pendingCase(test.title);
         log("##teamcity[testIgnored name='" + escape(test.title) + "' message='pending']");
     });
 
-    runner.on('pass', function(test) {
+    runner.on('pass', function (test) {
         allureReporter.endCase('passed');
         log("##teamcity[testFinished name='" + escape(test.title) + "' duration='" + test.duration + "']");
     });
 
-    runner.on('fail', function(test, err) {
+    runner.on('fail', function (test, err) {
         var status = err.name === 'AssertionError' ? 'failed' : 'broken';
-        if(global.onError) {
+        if (global.onError) {
             global.onError(err);
         }
         allureReporter.endCase(status, err);
@@ -56,18 +54,18 @@ function AllureReporter(runner, opts) {
 
 
 function escape(str) {
-  if (!str) return '';
-  return str
-    .toString()
-    .replace(/\|/g, "||")
-    .replace(/\n/g, "|n")
-    .replace(/\r/g, "|r")
-    .replace(/\[/g, "|[")
-    .replace(/\]/g, "|]")
-    .replace(/\u0085/g, "|x")
-    .replace(/\u2028/g, "|l")
-    .replace(/\u2029/g, "|p")
-    .replace(/'/g, "|'");
+    if (!str) return '';
+    return str
+        .toString()
+        .replace(/\|/g, "||")
+        .replace(/\n/g, "|n")
+        .replace(/\r/g, "|r")
+        .replace(/\[/g, "|[")
+        .replace(/\]/g, "|]")
+        .replace(/\u0085/g, "|x")
+        .replace(/\u2028/g, "|l")
+        .replace(/\u2029/g, "|p")
+        .replace(/'/g, "|'");
 }
 
 AllureReporter.prototype.__proto__ = Base.prototype;
